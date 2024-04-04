0c051287ccaf84c4a851c5d7134f811c520cc1bd
Luis Villas-Boas <shusten@hotmail.com>
2024-04-03 23:44:55 -0300

    :construction: Work in progress...

---
+++ algorithms/exec5.ts
@@ -0,0 +1,15 @@
+import prompt from 'prompt-sync';
+
+class SalaryAdjustment{
+    currentSalary: number; adjustmentSalary: number;
+
+    constructor(currentSalary: number, adjustmentSalary: number) {
+        this.currentSalary    =  currentSalary;
+        this.adjustmentSalary =  adjustmentSalary;
+    }
+
+    calculatePercentage(): void {
+        
+        
+    }
+}
